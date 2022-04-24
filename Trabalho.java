c1ae625a3ecc23a80f2e66be4680f8df48946a5c
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2022-04-24 18:09:40 -0300

    Update and rename README.md to Trabalho.java

---
+++ Trabalho.java
@@ -0,0 +1,70 @@
+public class Trabalho {
+
+    public static void main(String[] args) {
+
+        int[][] matriz = { {2,3},{4,5} };
+
+        int[][] rotation_90 = new int[2][2];
+        int[][] rotation_180 = new int[2][2];
+        int[][] rotation_270 = new int[2][2];
+
+        mostrar(matriz);
+
+        rotacionar_90_graus(matriz,rotation_90);
+        System.out.println();
+        mostrar(rotation_90);
+
+        rotacionar_180_graus(matriz,rotation_180);
+        System.out.println();
+        mostrar(rotation_180);
+
+        rotacionar_270_graus(matriz,rotation_270);
+        System.out.println();
+        mostrar(rotation_270);
+
+        int[][] aux=new int[2][2];
+
+        rotacionar_90_graus(rotation_270,aux);
+        System.out.println();
+        mostrar(aux);
+
+    }
+    public static void rotacionar_90_graus(int[][] a,int[][] b){
+        int z=2;
+        for(int i=0;i<a.length;i++){
+            for(int j=0; j<a[i].length ;j++){
+                b[i][j]=a[j][z];
+            }
+            z--;
+        }
+    }
+
+    public static void rotacionar_180_graus(int[][] a,int[][] b){
+        int z=2;
+        for(int i=0;i<a.length;i++){
+            int u=2;
+            for(int j=0; j<a[i].length ;j++){
+                b[i][j]=a[z][u];
+                u--;
+            }
+            z--;
+        }
+    }
+
+    public static void rotacionar_270_graus(int [][] a,int[][] b){
+        for(int i=0;i<a.length;i++){
+            for(int j=0,u=2; j<a[i].length; j++,u--){
+                b[i][j]=a[u][i];
+            }
+        }
+    }
+    public static void mostrar(int[][] a){
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length ;j++){
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+}

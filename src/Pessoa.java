09d928330701fb7ef4a7cd5acd610866cbe487c8
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2022-06-21 17:17:01 -0300

    Trabalho Finalizado Listas POO
    Trabalho faculdade 21/06 - Listas

---
+++ src/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+    private String nome;
+    private int codigo;
+
+    public Pessoa(String n, int i) {
+        this.setNome(n);
+        this.setCodigo(i);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + this.getNome() + "\nCodigo: " + this.getCodigo();
+    }
+}

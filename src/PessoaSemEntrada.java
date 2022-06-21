e75c63d3760e289482fd66188d320adbfdce18fe
Claudio Luiz Defrein Ionck <78597253+ClaudioIonck@users.noreply.github.com>
2022-06-21 17:38:18 -0300

    Trabalho 2 Pessoas com GetAll, Add, Delete - Final
    Resposta a considerar - Trabalho 2 POO

---
+++ src/PessoaSemEntrada.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.HashMap;
+public class PessoaSemEntrada {
+    public static void main(String[] args) {
+        HashMap<Integer,String> pessoa = new HashMap<>();
+        pessoa.put(1, "Cláudio");
+        pessoa.put(2, "Luiz");
+        pessoa.put(3, "Defrein");
+        pessoa.put(4, "Ionck" );
+        pessoa.put(5, null);
+
+        // todos os valores de pessoa
+        System.out.println("Lista de pessoas");
+        for(Map.Entry<Integer, String> entry : pessoa.entrySet()){
+
+            System.out.println("Numero total de pessoas: "+entry.getKey()+"Nome: "+entry.getValue());
+        }
+
+        System.out.println("Total de pessoas "+pessoa.size());
+        String antigo = pessoa.replace(1, "Claudio");
+        System.out.println("Nome alterado: "+pessoa.get(1));
+        String removido = pessoa.remove(2);
+        System.out.println("Pessoa excluida "+removido);
+
+        for(Map.Entry<Integer, String> entry : pessoa.entrySet()){
+            if(entry.getValue() == null){
+                System.out.println("Pessoa sem nome"+entry.getKey());
+            }
+        }
+
+        System.out.println("Lista de pessoas: ");
+        for(Map.Entry<Integer, String> entry : pessoa.entrySet()){
+            System.out.println("Numero de pessoas: "+entry.getKey()+" Nome: "+entry.getValue());
+        }
+
+
+
+
+
+
+
+    }
+}
